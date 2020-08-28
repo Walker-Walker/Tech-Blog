e8b1853f2d3188202679d74f2b702a0478c9e8d7
Walker-Walker <awalker0@gmail.com>
2020-08-27 19:48:39 -0700

    completed dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,14 +3,12 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
-
-// all get routes here 
+// all get routes here
 router.get("/", withAuth, (req, res) => {
   Post.findAll({
     where: {
       user_id: req.session.user_id,
-    }
-
+    },
   })
     .then((dbPostData) => {
       //serialize data before passing to template
@@ -23,10 +21,27 @@ router.get("/", withAuth, (req, res) => {
     });
 });
 
-router.get('/new', withAuth, (req, res) => {
-  res.render('new-post', {
-    layout: "dashboard"
-  })
-})
+// path for making a new post and sending it to the database
+router.get("/new", withAuth, (req, res) => {
+  res.render("new-post", {
+    // put new post inside of the dashboard handlebars
+    layout: "dashboard", // saying use this handlebars layout
+  });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findByPk(req.params.id)
+    .then((dbPostData) => {
+      if (dbPostData) {
+        const posts = dbPostData.get({ plain: true });
+        res.render("edit-post", { layout: dashboard, post });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
